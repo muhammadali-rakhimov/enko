84fdbe7dab62ef1d647160a51ba65ac8e696e04c
Muhammadali <muhammadali.rakhimov786@gmail.com>
2021-11-03 03:09:22 +0500

    updated

---
+++ form-to-email.php
@@ -0,0 +1,18 @@
+<?php
+  echo "Hello Everyone!"
+?>
+
+<!-- 
+
+  $name = $_POST['name'];
+  $phone_number = $_POST['number'];
+  
+	$email_from = 'starlight1234678@gmail.com';
+	$email_subject = "Email jo'natish";
+	$email_body = "You have received a new message from the user $name.\n"."Here is the message:\n $phone_number";
+
+  $to = "muhammadali.rakhimov786@gmail.com";
+  $headers = "Reply-To: Me \r\n";
+  mail($to, $email_subject, $email_body, $headers);
+
+ -->
